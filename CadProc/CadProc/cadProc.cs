ce446108cb353b2ab8f074dd8e6d7b40a9baeee7
killer9806 <killer980607@gmail.com>
2014-05-28 00:43:13 +0800

    asdfsd
    sdfsdftest

---
+++ CadProc/CadProc/cadProc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+
+using DNA;          // Dotnet6.0
+
+
+namespace CadProc
+{
+    public class cadProc
+    {
+        [CommandMethod("HelloWorld")]
+        public void HelloWorld()
+        {
+            Line line = new Line(new Point3d(0, 0, 0), new Point3d(100, 100, 0));       
+            Tools.AddToModelSpace(line);
+        }
+
+
+    }
+}
